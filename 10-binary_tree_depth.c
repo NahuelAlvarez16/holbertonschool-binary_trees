9d686260456a8be2ad207de0276dfc85da771f14
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-31 11:02:19 -0700

    Fix binary_tree_depth function.

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,24 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - measures the depth of a node in a binary tree
+ * binary_tree_depth_r - the depth of a node in a binary tree
+ * @tree: is a pointer to the node to measure the depth
+ * @is_first: 0 - Isn't first node | 1 - Is first node
+ * Return: return the depth of a node in a binary tree
+ */
+size_t binary_tree_depth_r(const binary_tree_t *tree, int is_first)
+{
+	int left_count = 0, right_count = 0;
+
+	if (!tree)
+		return (0);
+	return (!is_first + binary_tree_depth_r(tree, 0));
+}
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree 
  * @tree: is a pointer to the node to measure the depth
  * Return: return the depth of a node in a binary tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return (tree ? (!tree->left + !tree->right) : 0);
+	return (binary_tree_depth_r(tree, 1));
 }
