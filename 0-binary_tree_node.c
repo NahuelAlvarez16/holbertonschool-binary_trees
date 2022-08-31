4d70a72fac37330f8d8b20fd1d0a82f5caa0a214
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-31 08:33:05 -0700

    Fix memory of binary_tree_node function.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,15 +7,19 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *new = NULL;
 
 	new = malloc(sizeof(binary_tree_t));
 	if (!new)
 		return (NULL);
+	new->parent = NULL;
 	if (parent)
 	{
 		new->parent = parent;
 	}
+	new->right = NULL;
+	new->left = NULL;
 	new->n = value;
 	return (new);
 }
+
