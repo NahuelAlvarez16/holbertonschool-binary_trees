79c127e1403484e35d5aa482835a2b1ed03c5c8c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-31 12:46:03 -0700

    Fix binary_tree_leaves function.

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
 /**
  * binary_tree_leaves - that counts the leaves in a binary tree
- * @tree: count the number of leaves
- * Return: return count the number of leaves
+ * @tree:  a pointer to the root node of the tree to count the number of leaves
+ * Return: return count the leaves of a binary tree
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	int count = 0;
-	count = tree ? (!!tree->left + !!tree->right) : 0;
-	return ((count) ? count : tree ? 1 : 0);
+	int left_count = 0, right_count = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left_count = binary_tree_height_r(tree->left);
+	if (tree->right)
+		right_count = binary_tree_height_r(tree->right);
+	if (tree->left || tree->right)
+		return (left_count + right_count);
+	return (1);
 }
