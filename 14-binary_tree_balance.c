d4a267b0ec51622d146aa061a4678ea2cf1e2795
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-31 15:16:32 -0700

    WIP: Create binary_tree_balance function.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,18 +1,24 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_balance - that measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
+ * @is_first: is first node of the recursion
  * Return: that measures the balance factor of a binary tree
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree, int is_first)
 {
-	int count = 0;
+	int left_count = 0, right_count = 0;
 
 	if (!tree)
 		return (0);
 	if (tree->left)
-		count++;
+		left_count = binary_tree_balance(tree->left, 0);
 	if (tree->right)
-		count++;
-	return (count == 1 ? -1 : count);
+		right_count = binary_tree_balance(tree->right, 0);
+	if (!tree->left && !tree->right)
+		return (0);
+	if (is_first)
+		return (1 + left_count + right_count);
+	return (1 + left_count + right_count);
 }
