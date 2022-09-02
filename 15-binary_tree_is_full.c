78c0cabc11c179fbdd9b833529b462f954f8e901
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-09-02 06:53:17 -0700

    WIP: Create 15-binary_tree_is_full function.

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,10 +1,23 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - checks if a binary tree is full
+ * binary_tree_is_full_r - checks if a binary tree is full in recursive
  * @tree: is a pointer to the root node of the tree to check
+ * @is_first: is first node in the recursive function
  * Return: if 0, the node is incomplete, 1 the node is full
  */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_full_r(const binary_tree_t *tree, int is_first)
 {
-	return ((tree && ((tree->left && tree->right) || (!tree->left && !tree->right))) ? 1 : 0);
+	int is_incomplete = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		is_incomplete += binary_tree_is_full(tree->left);
+	if (tree->right)
+		is_incomplete += binary_tree_is_full(tree->right);
+	if (!tree->left ^ !tree->right)
+		return (1 + is_incomplete);
+	if (!tree->right && !tree->left)
+		return (0);
+	return (is_incomplete);
 }
